9bc2e1a6b95647c9a653318c8aa461fdba781996
bizzfuzz <ianmusau@gmail.com>
2024-08-16 11:07:27 +0300

    reading dummy data start

--- app/app.py
+++ app/app.py
@@ -1,5 +1,6 @@
 import pickle
 import uvicorn
+import pandas as pd
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi import FastAPI, HTTPException, Form, Request
@@ -23,6 +24,8 @@ with open('./data/death_preprocessor.pkl', 'rb') as file:
 with open('./data/readmission_preprocessor.pkl', 'rb') as file:
     readmission_preprocessor = pickle.load(file)
 
+patients = pd.read_csv("data/patients.csv")
+
 @app.get("/")
 async def read_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
@@ -30,6 +33,14 @@ async def read_root(request: Request):
 @app.get("/dashboard")
 async def dashboard(request: Request):
     return templates.TemplateResponse("dashboard.html", {"request": request})
+
+@app.get("/patients")
+async def view_patients(request: Request):
+    patients_temp = patients.to_dict(orient="records")
+    print(patients_temp)
+    return templates.TemplateResponse("patients.html", context={
+        "request": request, "patients": patients_temp
+    })
 
 if __name__ == '__main__':
     uvicorn.run(app, host='127.0.0.1', port=8000)
